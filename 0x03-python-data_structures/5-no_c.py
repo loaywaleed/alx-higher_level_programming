22f3d2b573de1b5b336dfa49deafdca7dea3d825
Loay Waleed <loaywaleed9@gmail.com>
2023-05-13 13:29:19 -0700

    Hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for i in my_string:
-        if i is not 'c' or i is not 'C':
+        if i is not 'c' and i is not 'C':
             new_string = new_string + i
     return new_string
