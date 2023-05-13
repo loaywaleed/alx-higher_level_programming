e58b5f664371fd308bf799b67e55c38095461065
Loay Waleed <loaywaleed9@gmail.com>
2023-05-13 04:27:14 -0700

    using string format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if f"{i:c}" != "q" and f"{i:c}" != "e":
-        print(f"{i:c}", end='')
+    if "{0:c}".format(i) != "q" and "{0:c}".format(i) != "e":
+        print("{0:c}".format(i), end='')
