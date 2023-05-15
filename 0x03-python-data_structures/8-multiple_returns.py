adceb54092e4b04724ae4da209f91107ac07af7c
Loay Waleed <loaywaleed9@gmail.com>
2023-05-15 14:43:36 -0700

    Handling empty string case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is none:
-        return 0, None
-    return len(sentence), sentence[0]
-
+    if len(sentence) == 0:
+        return 0, "None"
+    else:
+        return len(sentence), sentence[0]
