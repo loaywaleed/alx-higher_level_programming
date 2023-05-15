1fa0aa15d251f49cfd9b47b5a8906f95508c6d61
Loay Waleed <loaywaleed9@gmail.com>
2023-05-15 14:06:29 -0700

    Fixing coding style

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end='' if matrix[j] == matrix[len(matrix[i]) - 1] else ' ')
+            print("{:d}".format(matrix[i][j]), end=''
+                  if matrix[j] == matrix[len(matrix[i])-1] else ' ')
         print()
