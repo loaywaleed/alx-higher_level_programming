f072b8baa57e141f718431eee917943eb49eb8b8
Loay Waleed <loaywaleed9@gmail.com>
2023-05-17 13:21:30 -0700

    Hot fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary = {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     maximum = max(a_dictionary.values())
     for i, j in a_dictionary.items():
