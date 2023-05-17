99955298b758084c70c2d3820093ccec80a8d29a
Loay Waleed <loaywaleed9@gmail.com>
2023-05-17 12:57:43 -0700

    Hot fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for i in a_dictionary:
-        new_dict = a_dictionary[i] * 2
+        new_dict[i] = a_dictionary[i] * 2
     return new_dict
