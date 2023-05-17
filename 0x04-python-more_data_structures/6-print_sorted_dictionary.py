5442c937ece54e4ffe9ef5aea7af74a791264a24
Loay Waleed <loaywaleed9@gmail.com>
2023-05-17 12:23:53 -0700

    Changing printing format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sort_dict = sorted(a_dictionary)
     for i in sort_dict:
-        print("{:s} : {}".format(i, a_dictionary[i]))
+        print("{:s}: {}".format(i, a_dictionary[i]))
