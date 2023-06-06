162cfd533252ec6eb251da9d6799d9948c6226fa
root <loaywaleed9@gmail.com>
2023-06-06 09:11:40 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 def print_last_digit(number):
     if number < 0:
         number = number * -1
-    last_digit = number % 10;
+    last_digit = number % 10
     print(last_digit, end='')
     return last_digit
