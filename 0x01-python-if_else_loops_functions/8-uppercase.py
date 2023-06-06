6dfe76194f5adc1eaa4a50c9cfa360876e7caf07
root <loaywaleed9@gmail.com>
2023-06-06 08:30:22 -0700

    Using string format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     new_string = ""
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            new_string += chr(ord(i) - 32)
+            new_string += "{0:c}".format(ord(i) - 32)
         else:
             new_string += i
     print(new_string)
