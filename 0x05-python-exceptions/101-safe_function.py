ec99a4bbb980d4e4cc2a4fd4ddfb4c6838df1e88
root <loaywaleed9@gmail.com>
2023-06-26 11:39:23 -0700

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
