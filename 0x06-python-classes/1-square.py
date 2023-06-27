509c8079d16f2ea0e5ec0f14dbb67f9d1afa1812
root <loaywaleed9@gmail.com>
2023-06-27 12:31:49 -0700

    Fixing style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" Defines a class called Square """
-
+""" Defines a class called Square
+"""
 
 class Square():
     """ A square class
     Attributes:
-        __size: size of a side of the square
+        __size (int): size of a side of the square
     """
     def __init__(self, size):
         """ Initializes a square with private size
         Args:
-            size: size of side in the square
+            size (int): size of side in the square
         Returns: None
         """
         self.__size = size
