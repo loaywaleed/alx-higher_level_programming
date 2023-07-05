b6557c8390311357911c706bef3c17d39af01cf3
root <loaywaleed9@gmail.com>
2023-07-05 11:28:56 -0700

    Styling fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-
-Module that adds two integers a, b
-
+Python module
+adds two numbers
+returns result
 """
+
+
 def add_integer(a, b=98):
     """Adds two integers
     Args:
-        a, b: numbers to add"""
+        a, b: numbers to add
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
