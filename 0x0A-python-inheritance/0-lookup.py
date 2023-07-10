4de99b524c7abe693b21d7d1cd1d5f3457019afd
root <loaywaleed9@gmail.com>
2023-07-10 10:42:20 -0700

    Look-up function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""lookup function
+"""
+def lookup(obj):
+    """Args:
+        obj: takes in an object
+        return: a list of available attributes
+    """
+    return dir(obj)
