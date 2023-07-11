69311960b0289d83b6e3c7c734cf717beaee8f26
root <loaywaleed9@gmail.com>
2023-07-10 18:56:10 -0700

    square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""BaseGeometry class and subclass Rectangle"""
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """subclass that models a square"""
+    def __init__(self, size):
+        """int square object"""
+        integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """returns area of square"""
+        return self.__size * self.__size
