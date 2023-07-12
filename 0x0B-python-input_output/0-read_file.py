fde1041261dcc753af5996968328f9ed66bb9750
root <loaywaleed9@gmail.com>
2023-07-12 12:47:54 -0700

    Added documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""module that has read_file function"""
+
+
 def read_file(filename=""):
     """function that reads from a file"""
     with open(filename, encoding="utf-8") as my_file:
