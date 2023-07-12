9327dbfeedd37800514955beabb55534bb8feecd
root <loaywaleed9@gmail.com>
2023-07-12 13:32:14 -0700

    Changing documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""module that adds all arguments to a python list then save them to a file"""
+"""Script that adds all arguments to a python list then save them to a file"""
 
 from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
 
 try:
     my_list = load_from_json_file("add_item.json")
