c1cbffea7c1bf9a92324d66cd915c846e0af6724
root <loaywaleed9@gmail.com>
2023-07-12 14:38:34 -0700

    replacing attributes of the class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module that has student class"""
+
+
+class Student:
+    """class that models a student"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict respresentation of a student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """replaces attributes of the student instance"""
+        for el in json:
+            try:
+                setattr(self, el, json[i])
+            except FileNotFoundError:
+                pass
