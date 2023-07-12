d0f50cf5ca393fb8a6e1e8885c1c7762aa473a7d
root <loaywaleed9@gmail.com>
2023-07-12 15:22:34 -0700

    Pascal Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module that has pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """function that returns pascal triangle"""
+    my_list = [[1]]
+
+    for i in range(0, n - 1):
+        temp = [0] + my_list[-1] + [0]
+        row = []
+        for j in range(len(my_list[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        my_list.append(row)
+
+    return my_list
