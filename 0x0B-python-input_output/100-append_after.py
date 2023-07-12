2edafa0a9d017698f41d64bf6647c51ead6fe3e5
root <loaywaleed9@gmail.com>
2023-07-12 15:31:57 -0700

    Appending after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Modules that inserts string with a file after a specified string is reached"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that appends after a searched string"""
+    text = ""
+    with open(filename, encoding="utf-8") as my_file:
+        for line in my_file:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as new_file:
+        new_file.write(text)
