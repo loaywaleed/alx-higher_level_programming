c4656d53620d0f57ad2ee921e13fcc8c1f0d0112
Loay Waleed <loaywaleed9@gmail.com>
2023-07-15 21:28:05 +0300

    Added base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that contains Base class"""
+
+
+class Base:
+    """Base class for other subclasses"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Instantiation of an object"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
