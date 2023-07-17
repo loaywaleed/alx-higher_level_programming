0e97a33ccd23d43c3fab032c95787d70f67c2e48
Loay Waleed <loaywaleed9@gmail.com>
2023-07-17 11:47:03 +0300

    Added to dictionary method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -112,5 +112,11 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-
-
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }        
