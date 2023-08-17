0cec72d6f75794eb3675e6f425f8eb457be45bb3
loay <loaywaleed9@gmail.com>
2023-08-17 23:54:04 +0300

    hot fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script that lists all cities in a ceratin database
-SELECT cities.id, cities.name, states.name
+SELECT c.id, c.name, s.name
 FROM cities AS c
 INNER JOIN states AS s
 ON c.state_id = s.id
