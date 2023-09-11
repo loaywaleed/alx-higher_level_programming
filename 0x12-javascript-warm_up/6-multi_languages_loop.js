c1c962453d6d44288f3263ad79600e34e3ebbff3
loay <loaywaleed9@gmail.com>
2023-09-11 22:24:07 +0300

    looping

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* Looping through multilines */
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; myArr[i]; i++) {
+  console.log(myArr[i]);
+}
