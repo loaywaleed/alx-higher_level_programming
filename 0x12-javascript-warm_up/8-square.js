8d0050fa3e5ecbb19b08b07828995a020571f778
loay <loaywaleed9@gmail.com>
2023-09-11 22:51:17 +0300

    hot fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* Printing a square */
 
-args = process.argv
+const args = process.argv;
 
 if (args[2] && !(isNaN(args[2]))) {
   for (let i = 0; i < args[2]; i++) {
-      console.log('X'.repeat(args[2]));
+    console.log('X'.repeat(args[2]));
   }
 } else {
   console.log('Missing size');
