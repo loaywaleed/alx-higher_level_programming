f64316883019e1a3bccd9869bf49907201b53cc1
loay <loaywaleed9@gmail.com>
2023-09-12 22:13:58 +0300

    printing square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* printing method */
+
+const Rectangle = require('./4-rectangle');
+
+/* Square class */
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'x';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
