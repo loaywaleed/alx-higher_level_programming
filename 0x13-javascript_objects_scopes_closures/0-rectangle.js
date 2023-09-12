67b07af71bab403c6e68c1673a57ad72103ed2d6
loay <loaywaleed9@gmail.com>
2023-09-12 20:45:59 +0300

    Defining width and height

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 /* Class rectangle */
 
-module.exports = class Rectangle {}
+module.exports = class Rectangle {};
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Defining rectangle */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
