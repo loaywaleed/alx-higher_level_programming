025bb82b13f49419b7bbe4848bef080e625d2d13
loay <loaywaleed9@gmail.com>
2023-09-16 12:13:29 +0300

    Listing states starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-usr/bin/python3
+#!/usr/bin/python3
 """Script that lists all states"""
 
 from sys import argv
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                          host='localhost', port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     states = cursor.fetchall()
     for state in states:
