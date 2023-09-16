18753ca718cefc43323765726c27813695762771
loay <loaywaleed9@gmail.com>
2023-09-16 12:46:09 +0300

    filtering safely to avoid SQLI

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that lists all states"""
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         host='localhost', port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name\
+                   LIKE BINARY %s ORDER BY id ASC", [argv[4]])
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
