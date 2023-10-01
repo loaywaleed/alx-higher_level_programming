4f83f0cb87865d1e1b7461e7112ed0465b5864fd
loay <loaywaleed9@gmail.com>
2023-10-01 14:21:20 +0300

    hot fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that that posts two parameters
-curl -sXd POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
