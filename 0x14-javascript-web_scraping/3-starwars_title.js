3e34c088953a69764ff402fe6fc31ac3f79f5334
= <=>
2023-10-24 20:43:30 +0300

    hotfix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,9 @@ const request = require('request');
 const argv = process.argv;
 
 request(`https://swapi-api.alx-tools.com/api/films/${argv[2]}`, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }
-    const titleOfFilm = JSON.parse(body).title;
-    console.log(titleOfFilm);
+  if (error) {
+    console.log(error);
+  }
+  const titleOfFilm = JSON.parse(body).title;
+  console.log(titleOfFilm);
 });
